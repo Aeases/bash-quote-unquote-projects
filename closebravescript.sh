26e2afb4418b51b672d705c4593ae7566f0e069f
Aeases <96164686+Aeases@users.noreply.github.com>
2022-11-22 20:27:36 +0800

    initial

---
+++ closebravescript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+directory=$(pwd)
+export DISPLAY=:0
+export GNOME_DESKTOP_SESSION_ID=0
+
+file=closebravescript.sh
+ie=1
+
+while [ -f "$file" ]
+do
+	killall brave
+	if [ $? -eq 0 ]; then
+	zenity --warning --text "get annoyed" --title "$directory" --width 600
+	fi
+done
+while [ $ie -lt 3 ];
+do
+	notify-send -u critical "$directory" "ERROR DOES NOT EXIST" -t 2000
+	let "ie=ie+1"
+	echo "hi"
+done
+
